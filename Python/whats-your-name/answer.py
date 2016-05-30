68674e6fb0f6b1f0eab888aa03c090baf40fc720
Fed <fedmich@gmail.com>
2016-05-30 19:13:25 +0800

    SprintF in Python

---
+++ Python/whats-your-name/answer.py
@@ -0,0 +1,9 @@
+def solution( ):
+	first = raw_input()
+	last = raw_input()
+
+	print ( "Hello %s %s! You just delved into python." % ( first , last ) )
+
+if __name__ == "__main__":
+	solution( )
+	
