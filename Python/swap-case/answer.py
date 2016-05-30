42c708d669138c0187a5e7d65e9babaff2b7fdb5
Fed <fedmich@gmail.com>
2016-05-30 18:29:08 +0800

    This one is very easy as they didn't even restricted the use of swapcase() function

---
+++ Python/swap-case/answer.py
@@ -0,0 +1,11 @@
+'''
+You are given a string . Your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+'''
+
+def solution(a ):
+	return a.swapcase()
+
+if __name__ == "__main__":
+	s = raw_input()
+	answer = solution( s)
+	print ( answer )
